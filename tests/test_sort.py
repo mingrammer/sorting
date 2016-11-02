07e0d52cd18dbaa689f1e34b0f3bb7476dce1544
mingrammer <k239507@gmail.com>
2016-11-02 23:39:58 +0900

    Add unittest for mergesort

---
+++ tests/test_sort.py
@@ -0,0 +1,16 @@
+import unittest
+
+from sorts.mergesort import mergesort
+
+
+class TestSort(unittest.TestCase):
+    def test_mergesort(self):
+        arr = [10, 1, 4, 3, 8, 2, 7, 9, 5, 6]
+
+        arr = mergesort(arr)
+
+        self.assertEqual(arr, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+
+
+if __name__ == '__main__':
+    unittest.main()
