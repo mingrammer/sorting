1625d35bffa91877f50fe8151e066fc0a0833ba6
mingrammer <k239507@gmail.com>
2016-11-03 15:16:43 +0900

    Add unittest for heapsort

--- tests/test_sort.py
+++ tests/test_sort.py
@@ -1,9 +1,17 @@
 import unittest
 
+from algorithms.heapsort import heapsort
 from algorithms.mergesort import mergesort
 
 
 class TestSort(unittest.TestCase):
+    def test_heapsort(self):
+        arr = [10, 1, 4, 3, 8, 2, 7, 9, 5, 6]
+
+        arr = heapsort(arr)
+
+        self.assertEqual(arr, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+
     def test_mergesort(self):
         arr = [10, 1, 4, 3, 8, 2, 7, 9, 5, 6]
 
